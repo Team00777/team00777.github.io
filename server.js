afc30365458d573cc2187a7d73b968b43770a95c
AlexanderSQRT <dirtypurpledove@gmail.com>
2022-01-04 22:18:41 +0200

    updated server configuration

--- server.js
+++ server.js
@@ -49,4 +49,54 @@ app.use((err, req, res, next) => {
   }
 })
 
+const fs = require('fs');
+const nodemailer = require("nodemailer");
+const urlencodedParser = express.urlencoded({extended: false});
+
+app.post("/", urlencodedParser, (req, res) => {
+
+    if(!req.body) return res.sendStatus(400);
+    console.log(req.body);
+    const enteredEmailAddress = req.body.userEmail;
+
+    fs.appendFile('emailAddresses.txt', `\n${enteredEmailAddress}`, (error) => {
+      console.log(error);
+    });
+    console.log(`Email address ${enteredEmailAddress} has been saved to emailAddresses.txt`);
+
+      async function sendEmail() {
+
+        try{
+
+          let transporter = nodemailer.createTransport({
+            host: "smtp.ethereal.email",
+            port: 587,
+            secure: false,
+            auth: {
+              user: "hailee.kunde99@ethereal.email",
+              pass: "sEEntMMKqJrKJfryfZ",
+            },
+          });
+
+          const emailSettings = {
+            from: '"Fred Foo 👻" <foo@example.com>',
+            to: `${enteredEmailAddress}`,
+            subject: "Thank you!",
+            html: "<b>Thank you for subscription!</b>",
+          }
+          
+          let sentEmail = await transporter.sendMail(emailSettings, (err) => {
+            if (err) {
+              console.log("Error " + err);
+            } else {
+              console.log("Email sent successfully");
+            }
+          });
+        
+        } catch (error) {
+          console.error(error);
+        }
+}
+});
+
 app.listen(8000, () => console.info("Server is running..."));
