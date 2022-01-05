4a6369f73be1b3d4877dd7494f01241fe5102de4
AlexanderSQRT <dirtypurpledove@gmail.com>
2022-01-05 23:13:55 +0200

    updated server configuration

--- server.js
+++ server.js
@@ -7,43 +7,45 @@ const nodemailer = require("nodemailer");
 
 const app = express();
 
-
 app.use(express.static("public"));
 
 app.get("/", (req, res, next) => {
-  ejs.renderFile("./src/index.ejs", {enteredEmailAddress: ""}, function(err, str){
 
-    if(err) {
-      next(err);
-    return;
-    }
-    
-    res.send(str);
+    ejs.renderFile("./src/index.ejs", {enteredEmailAddress: ""}, function(err, str){
+
+      if(err) {
+        next(err);
+      return;
+      }
+      
+      res.send(str);
 });
 });
 
 app.get("/career", (req, res, next) => {
-  ejs.renderFile("./src/pages/career.ejs", function(err, str) {
 
-    if(err) {
-      next(err);
-    return;
-    }
-    
-    res.send(str);
-  });
+    ejs.renderFile("./src/pages/career.ejs", function(err, str) {
+
+      if(err) {
+        next(err);
+      return;
+      }
+      
+      res.send(str);
+    });
 });
 
 app.get("/story", (req, res, next) => {
-  ejs.renderFile("./src/pages/story.ejs", function(err, str) {
 
-    if(err) {
-      next(err);
-    return;
-    }
-    
-    res.send(str);
-  });
+    ejs.renderFile("./src/pages/story.ejs", function(err, str) {
+
+      if(err) {
+        next(err);
+      return;
+      }
+      
+      res.send(str);
+    });
 });
 
 const urlencodedParser = express.urlencoded({extended: false});
@@ -51,27 +53,26 @@ const emailValidity = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 app.post("/sendEmail", urlencodedParser, async (req, res, next) => {
 
-    console.log(req.body.userEmail);
     const enteredEmailAddress = req.body?.userEmail;
 
-    if(!enteredEmailAddress) {
+    if (!enteredEmailAddress) {
       return next(new Error("Email was not entered"));
-    } else if(!emailValidity.test(enteredEmailAddress)) {
+
+    } else if (!emailValidity.test(enteredEmailAddress)) {
       return next(new Error("Email is invalid"));
     };
 
-    fs.appendFile('emailAddresses.txt', `\n${enteredEmailAddress}`, (error) => {
-      console.log(error);
-    });
-    console.log(`Email address ${enteredEmailAddress} has been saved to emailAddresses.txt`);
+    fs.appendFile('emailAddresses.txt', `\n${enteredEmailAddress}`, (error) => console.log(error));
 
   try {
 
     await sendEmail(enteredEmailAddress);
+    
     const htmlMainPage = await ejs.renderFile("./src/index.ejs", {enteredEmailAddress});
     res.send(htmlMainPage);
 
   } catch (error) {
+
     next(error);
   }
 
@@ -79,24 +80,32 @@ app.post("/sendEmail", urlencodedParser, async (req, res, next) => {
 
 async function sendEmail(enteredEmailAddress) {
 
-    let transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-        user: "",
-        pass: '',
-        },
-    });
+  let transporter = nodemailer.createTransport({
 
-    const emailSettings = {
-      from: '"Company DB-" <...>',
+      service: 'gmail',
+      auth: {
+      user: "",
+      pass: '',
+      },
+  });
+
+  const emailSettings = {
+    
+      from: '"Company DB-" <>',
       to: `${enteredEmailAddress}`,
       subject: "Thank you for subscription!",
-      html: "<b>Thank you for subscription!</b>",
+      html: `<p>Hello, you've just subscribed for updates on our website.</p> 
+              <p>We will keep you updated on our services, discounts and many more!</p>
+              <p>The emails will be sent to you every week. You may unsubscribe anytime.</p>
+              <p>Thank you for subscription!</p>
+              <p>Respectfully, Company DB-</p>`,
     }
     
-    let sentEmail = await transporter.sendMail(emailSettings, (err) => {
+  let sentEmail = await transporter.sendMail(emailSettings, (err) => {
+
       if (err) {
         console.log("Error " + err);
+
       } else {
         console.log("Email sent successfully");
       }
@@ -104,10 +113,11 @@ async function sendEmail(enteredEmailAddress) {
 }
 
 app.use((err, req, res, next) => {
-  if(err) {
-    res.send(err.message);
-    console.error(err);
-  }
+
+    if(err) {
+      res.send(err.message);
+      console.error(err);
+    }
 })
 
 app.listen(8000, () => console.info("Server is running..."));
